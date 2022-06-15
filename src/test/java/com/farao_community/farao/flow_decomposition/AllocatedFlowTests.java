e5d456c67eff83d5f9e9a5bc4647c110dbaa1561
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2022-06-15 10:39:42 +0200

    Refactor network importer test

--- src/test/java/com/farao_community/farao/flow_decomposition/AllocatedFlowTests.java
+++ src/test/java/com/farao_community/farao/flow_decomposition/AllocatedFlowTests.java
@@ -7,10 +7,7 @@
 package com.farao_community.farao.flow_decomposition;
 
 import com.powsybl.iidm.import_.Importers;
-import com.powsybl.iidm.network.Branch;
-import com.powsybl.iidm.network.Generator;
-import com.powsybl.iidm.network.Load;
-import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -24,19 +21,29 @@ class AllocatedFlowTests {
     private static final double EPSILON = 1e-3;
 
     @Test
-    void checkThatBasicNetworkIsWellImported() {
+    void checkThatBasicNetworkIsWellImported_TestFAR670_06() {
         String networkFileName = "NETWORK_SINGLE_LOAD_TWO_GENERATORS_WITH_COUNTRIES.uct";
         NetworkInterface networkInterface = new NetworkInterface(networkFileName);
         Network basicNetwork = networkInterface.getNetwork();
+        assertNotNull(basicNetwork);
+
         Branch<?> branchWithGeneratorOnTerminal1 = basicNetwork.getBranch("FGEN1 11 BLOAD 11 1");
         Branch<?> branchWithGeneratorOnTerminal2 = basicNetwork.getBranch("BLOAD 11 BGEN2 11 1");
+        assertNotNull(branchWithGeneratorOnTerminal1);
+        assertNotNull(branchWithGeneratorOnTerminal2);
+
         Generator generatorFromBranch1 = basicNetwork.getGenerator("FGEN1 11_generator");
         Generator generatorFromBranch2 = basicNetwork.getGenerator("BGEN2 11_generator");
         Load centralLoad = basicNetwork.getLoad("BLOAD 11_load");
-        assertNotNull(branchWithGeneratorOnTerminal1);
-        assertNotNull(branchWithGeneratorOnTerminal2);
         assertNotNull(generatorFromBranch1);
         assertNotNull(generatorFromBranch2);
         assertNotNull(centralLoad);
+
+        Bus busWithGenerator1 = basicNetwork.getBusBreakerView().getBus("FGEN1 11");
+        Bus busWithGenerator2 = basicNetwork.getBusBreakerView().getBus("BGEN2 11");
+        Bus busWithLoad = basicNetwork.getBusBreakerView().getBus("BLOAD 11");
+        assertNotNull(busWithGenerator1);
+        assertNotNull(busWithGenerator2);
+        assertNotNull(busWithLoad);
     }
 }
