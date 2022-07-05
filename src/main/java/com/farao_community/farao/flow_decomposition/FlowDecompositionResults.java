d96138f5e9d95f6eb045a754a2f5fae969e2d63c
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2022-07-05 09:09:28 +0200

    FlowDecompositionResults documentation

--- src/main/java/com/farao_community/farao/flow_decomposition/FlowDecompositionResults.java
+++ src/main/java/com/farao_community/farao/flow_decomposition/FlowDecompositionResults.java
@@ -13,21 +13,34 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
+ * This class provides flow decomposition results from a network.
+ * Those results are returned by a flowDecompositionComputer when run on a network.
+ * By default, the results only contain the flow decomposition of the XNECs.
+ * If this runner has its argument {@code saveIntermediate} set to {@code true},
+ * then the results will contain supplementary information.
+ *
  * @author Sebastien Murgey {@literal <sebastien.murgey at rte-france.com>}
  * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
+ * @see FlowDecompositionComputer
+ * @see DecomposedFlow
  */
 public class FlowDecompositionResults {
     private final boolean saveIntermediates;
     private SparseMatrixWithIndexesCSC decomposedFlowsMatrix;
     private Map<Country, Map<String, Double>> glsks;
-    private SparseMatrixWithIndexesTriplet nodalInjectionsMatrix;
     private SparseMatrixWithIndexesTriplet ptdfMatrix;
+    private SparseMatrixWithIndexesTriplet nodalInjectionsMatrix;
     private Map<String, Double> dcNodalInjections;
 
     FlowDecompositionResults(boolean saveIntermediates) {
         this.saveIntermediates = saveIntermediates;
     }
 
+    /**
+     * @param fillZeros Ignore the sparse property of the flow decomposition.
+     *                  It fills blanks with zeros.
+     * @return A flow decomposition map. The keys are the XNEC ids and the values are {@code DecomposedFlow} objects.
+     */
     public Map<String, DecomposedFlow> getDecomposedFlowsMap(boolean fillZeros) {
         return decomposedFlowsMatrix.toMap(fillZeros).entrySet()
             .stream()
@@ -38,6 +51,58 @@ public class FlowDecompositionResults {
 
     public Map<String, DecomposedFlow> getDecomposedFlowsMap() {
         return getDecomposedFlowsMap(false);
+    }
+
+    /**
+     * GLSKs are an intermediate results.
+     * They are represented as a sparse map of map.
+     * The first key is a zone, the second key is a node id and the value is the GLSK of the node in the country.
+     * They will be saved if this runner has its argument {@code saveIntermediate} set to {@code true}.
+     * @return An optional containing GLSKs.
+     */
+    public Optional<Map<Country, Map<String, Double>>> getGlsks() {
+        return Optional.ofNullable(glsks);
+    }
+
+    /**
+     * PTDFs are an intermediate results.
+     * They will be saved if this runner has its argument {@code saveIntermediate} set to {@code true}.
+     * They are represented as a sparse map of map.
+     * The first key is a XNEC id, the second key is a node id and the value is the PTDF.
+     * @return An optional containing PTDFs
+     */
+    public Optional<Map<String, Map<String, Double>>> getPtdfMap() {
+        return Optional.ofNullable(ptdfMatrix).map(SparseMatrixWithIndexesTriplet::toMap);
+    }
+
+    /**
+     * Nodal injections are an intermediate results.
+     * They will be saved if this runner has its argument {@code saveIntermediate} set to {@code true}.
+     * They are represented as a sparse map of map.
+     * The first key is a node id, the second key is a column identifier and the value is the nodal injection.
+     * The one of the column id is the {@code "Allocated"}. It corresponds to the allocated nodal injection.
+     * The other column ids are Zone Ids as Strings. Each column corresponds to the nodal injection in this zone.
+     * @param fillZeros Ignore the sparse property of the nodal injections.
+     *                  It fills blanks with zeros.
+     * @return An optional containing nodal injections
+     */
+    public Optional<Map<String, Map<String, Double>>> getNodalInjectionsMap(boolean fillZeros) {
+        return Optional.ofNullable(nodalInjectionsMatrix).map(matrix -> matrix.toMap(fillZeros));
+    }
+
+    public Optional<Map<String, Map<String, Double>>> getNodalInjectionsMap() {
+        return getNodalInjectionsMap(false);
+    }
+
+    /**
+     * DC Nodal injections are an intermediate results.
+     * They will be saved if this runner has its argument {@code saveIntermediate} set to {@code true}.
+     * They are represented as a map.
+     * The key is a node id and the value is the DC nodal injection.
+     * @return An optional containing DC nodal injections
+     */
+    public Optional<Map<String, Double>> getDcNodalInjectionsMap() {
+        return Optional.ofNullable(dcNodalInjections);
     }
 
     private DecomposedFlow flowPartsMapToDecomposedFlow(Map<String, Double> flowPartsMap) {
@@ -66,27 +131,7 @@ public class FlowDecompositionResults {
         }
     }
 
-    Optional<Map<Country, Map<String, Double>>> getGlsks() {
-        return Optional.ofNullable(glsks);
-    }
-
-    Optional<Map<String, Map<String, Double>>> getNodalInjectionsMap(boolean fillZeros) {
-        return Optional.ofNullable(nodalInjectionsMatrix).map(matrix -> matrix.toMap(fillZeros));
-    }
-
-    Optional<Map<String, Map<String, Double>>> getNodalInjectionsMap() {
-        return getNodalInjectionsMap(false);
-    }
-
-    Optional<Map<String, Map<String, Double>>> getPtdfMap() {
-        return Optional.ofNullable(ptdfMatrix).map(SparseMatrixWithIndexesTriplet::toMap);
-    }
-
-    Optional<Map<String, Double>> getDcNodalInjectionsMap() {
-        return Optional.ofNullable(dcNodalInjections);
-    }
-
-    public void saveDcNodalInjections(Map<String, Double> dcNodalInjections) {
+    void saveDcNodalInjections(Map<String, Double> dcNodalInjections) {
         this.dcNodalInjections = dcNodalInjections;
     }
 }
