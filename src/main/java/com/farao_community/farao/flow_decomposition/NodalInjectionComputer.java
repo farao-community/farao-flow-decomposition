84d7d3f64dfcb5e1eb9baf8db5d635c201f78148
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2022-07-06 15:49:31 +0200

    Extract PstFlowComputer

--- src/main/java/com/farao_community/farao/flow_decomposition/NodalInjectionComputer.java
+++ src/main/java/com/farao_community/farao/flow_decomposition/NodalInjectionComputer.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.flow_decomposition;
 
 import com.powsybl.commons.PowsyblException;
@@ -10,6 +16,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
+ */
 class NodalInjectionComputer {
     private static final double DEFAULT_GLSK_FACTOR = 0.0;
     private static final String ALLOCATED_COLUMN_NAME = "Allocated Flow";
